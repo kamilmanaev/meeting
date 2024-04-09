71795dd4e23d2b6f65c08599445d9d90a3bdcd25
kamilmanaev <kamilmanaev175@gmail.com>
2024-04-09 19:40:47 +0500

    создание

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import API from "../API";
 export const Users = () => {
-  console.log(API.users.fetchAll());
+  const [users, setUsers] = useState(API.users.fetchAll());
+  console.log(users);
   return <h1>name</h1>;
 };
